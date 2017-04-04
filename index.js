4fef545489b2d7dadf1e092885f7230140dff2a9
Toy <1659808224@qq.com>
2017-04-04 12:59:18 +0800

    change

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 module.exports = function() {}
 module.exports.pitch=function(remainingRequest){
-	if(this.target=="node"){
-		return `module.exports = require("${"-!"+remainingRequest}").default`
-	}else{	
+	// if(this.target=="node"){
+	// 	return `module.exports = require("${"-!"+remainingRequest}").default`
+	// }else{
 		return `
 var wait = require("ssr-loader/waiter")
 module.exports=wait.default(function(){return import("${"-!"+remainingRequest}")})
 `
-	}
+	// }
 }
